5b78a5cc85412e88aa047449820664dc1c640b2f
Bhawana <bhawanachalise626@gmail.com>
2026-04-22 22:22:28 +0545

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import s from './Footer.module.css';
 export default function Footer(){
   return(
     <footer className={s.footer}>
-      <p className={s.copy}>© {new Date().getFullYear()} Bhawana Chalise — Graphic &amp; UI Designer. All rights reserved.</p>
+      <p className={s.copy}>© {new Date().getFullYear()} Bhabana Chalise — Graphic &amp; UI Designer. All rights reserved.</p>
     </footer>
   );
 }
